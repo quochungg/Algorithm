c838a59aab9895d144a8d3538a4546879b72ae1b
NHQHwng <nguyenhung.5703@gmail.com>
2024-03-10 16:22:12 +0700

    My First Commit

---
+++ src/StoneOnTheTable.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class StoneOnTheTable {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int result = 0;
+        int quantity = sc.nextInt();
+        sc.nextLine();
+        String color = sc.nextLine();
+        for(int i = 0; i <quantity-1; i++){
+            if (color.charAt(i)==color.charAt(i+1)){
+                result++;
+            }
+        }
+        System.out.println(result);
+        sc.close();
+    }
+}
