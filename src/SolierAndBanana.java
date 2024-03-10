24d8ac00cda356c53af005d00dbaf0f915f3bc44
NHQHwng <nguyenhung.5703@gmail.com>
2024-03-10 16:45:58 +0700

    Solier And Banana

---
+++ src/SolierAndBanana.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+/**
+ * SolierAndBanana
+ */
+public class SolierAndBanana {
+
+    public static void main(String[] args) {
+        int n,w,k;
+        Scanner sc = new Scanner(System.in);
+        k = sc.nextInt(); //the cost of the first banana
+        n = sc.nextInt(); //initial number of dollars
+        w = sc.nextInt(); //number of bananas he wants
+        
+        for (int i = 1; i <= w ; i++) {
+            n -= (i*k);
+        }
+        int result;
+        if (n >= 0){
+            result = 0;
+        }else{
+            result = n*-1;
+        }
+        System.out.println(result);
+        
+    }
+}
