a8d59c329611a9e4bde104e87ad2958064b5daac
NHQHwng <nguyenhung.5703@gmail.com>
2024-03-11 23:23:31 +0700

    Hulk

--- src/Hulk.java
+++ src/Hulk.java
@@ -4,14 +4,15 @@ public class Hulk {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for (int i = 1; i <= n ; i++) {
+        for (int i = 1; i <= n; i++) {
             if (i % 2 == 0) {
                 System.out.print("I love ");
-            }else{
+            } else {
                 System.out.print("I hate ");
-            }if(i!=n){
+            }
+            if (i != n) {
                 System.out.print("that ");
-            }else{
+            } else {
                 System.out.print("it ");
             }
         }
