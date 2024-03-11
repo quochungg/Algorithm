219cae85a3c89070b099d8c4d2e129e168de0f22
NHQHwng <nguyenhung.5703@gmail.com>
2024-03-11 23:08:29 +0700

    DominoPiling

--- src/DominoPiling.java
+++ src/DominoPiling.java
@@ -1,3 +1,11 @@
+import java.util.Scanner;
+
 public class DominoPiling {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int result = (m * n) / 2;
+        System.out.println(result);
+    }
 }
